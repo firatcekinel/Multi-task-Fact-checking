0e44a6124f245e621b602958b2207edfb70732f8
firatcekinel <88368345+firatcekinel@users.noreply.github.com>
2023-08-07 11:08:36 +0200

    Update t5_summarization.py

--- t5_summarization.py
+++ t5_summarization.py
@@ -337,15 +337,6 @@ for i in tqdm(range(test_df.shape[0])):
     model_out.append(summarizeText(text, model))
     reference.append(str(row['summary']))
 
-"""
-model_out = ["he began by starting a five person war cabinet and included chamberlain as lord president of the council",
-             "the siege lasted from 250 to 241 bc, the romans laid siege to lilybaeum",
-             "the original ocean water was found in aquaculture"]
-
-reference = ["he began his premiership by forming a five-man war cabinet which included chamberlain as lord president of the council",
-             "the siege of lilybaeum lasted from 250 to 241 bc, as the roman army laid siege to the carthaginian-held sicilian city of lilybaeum",
-            "the original mission was for research into the uses of deep ocean water in ocean thermal energy conversion (otec) renewable energy production and in aquaculture"]
-"""
 rouge = Rouge()
 
 print(rouge.get_scores(model_out, reference, avg=True))
